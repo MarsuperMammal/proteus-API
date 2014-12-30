ada575a82b89ebb1def2ba2f34a1dfa328fc4d1f
te206676 <philip.watts@te.com>
2014-12-29 19:20:56 -0500

    Update proteus-destroy.pl

--- proteus-destroy.pl
+++ proteus-destroy.pl
@@ -7,7 +7,7 @@ use SOAP::Lite;
 
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME}=0;
 my $cfg_proteus_api_user = "TE0S0067";
-my $cfg_proteus_api_password = "/Thgq0*0wa";
+my $cfg_proteus_api_password = "";
 
 $service = Service->
   connect("address" => '135.107.21.203', "enableSSL" => 'true' );
