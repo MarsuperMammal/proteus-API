283cf09b79f753b2328038c0c2f4741d91ce310d
Phil Watts <philip.watts@te.com>
2015-02-07 11:11:16 -0500

    code

--- proteus-test.pl
+++ proteus-test.pl
@@ -7,10 +7,10 @@ use LWP::Simple qw(get);
 use Data::Dumper;
 $ENV{HTTPS_CA_FILE} = "/usr/share/centrifydc/apache/certs/ca-certs.crt";
 $service = Service->connect("address" => '135.107.21.203', "enableSSL" => 'true' );
-$hostname = `facter hostname`;
 $fqdn = `facter fqdn`;
+@aliases = split(/,/, `facter alias`);
 $ipaddr = `facter ipaddress`;
-chomp ($fqdn, $ipaddr, $hostname);
+chomp ($fqdn, $ipaddr, @aliases);
 
 ## log in and establish a session
 $service->login(
@@ -20,11 +20,20 @@ SOAP::Data->name( 'password' )->value( '/Thgq0*0wa' )->type('string')->attr({xml
 @host_record = $service->getHostRecordsByHint(SOAP::Data->type( 'int' )->name( 'start' )->value( 0 )->attr({xmlns => ''}),
 SOAP::Data->type( 'int' )->name( 'count' )->value( 1 )->attr({xmlns => ''}),
 SOAP::Data->type( 'string' )->name( 'options' )->value( "hint=$fqdn" )->attr({xmlns => ''}))->result;
-print "dump of host_record:\n", Dumper(@host_record),"\n";
-
-
-@linkedRecords = $service->getLinkedEntities(SOAP::Data->type( 'long' )->name( 'entityId' )->value( $fqdn )->attr({xmlns => ''}),
-SOAP::Data->type( 'string' )->name( 'type' )->value( ObjectTypes::RecordWithLink )->attr({xmlns => ''}),
-SOAP::Data->type( 'int' )->name( 'start' )->value( 0 )->attr({xmlns => ''}),
-SOAP::Data->type( 'int' )->name( 'count' )->value( 5 )->attr({xmlns => ''}))->valueof( '//getLinkedEntitiesResponse/return/item' );
-print "dump of linkedRecords:\n", Dumper(@linkedRecords), "\n";
+$host_id=$host_record[0]{item}{id};
+if(defined($host_id)){
+    @linkedRecords = $service->getLinkedEntities(SOAP::Data->type( 'long' )->name( 'entityId' )->value( $host_id )->attr({xmlns => ''}),
+    SOAP::Data->type( 'string' )->name( 'type' )->value( ObjectTypes::RecordWithLink )->attr({xmlns => ''}),
+    SOAP::Data->type( 'int' )->name( 'start' )->value( 0 )->attr({xmlns => ''}),
+    SOAP::Data->type( 'int' )->name( 'count' )->value( 5 )->attr({xmlns => ''}))->valueof( '//getLinkedEntitiesResponse/return/item' );
+    print "dump of linkedRecords:\n", Dumper(@linkedRecords), "\n";
+$n = 0;
+while (defined($linkedRecords[$n]{name})) {
+  $links=$linkedRecords[$n]{name};
+  if($links =~ /^(.*)\d{3}$/) {
+  $seen{$1}++; 
+  }
+  $n=$n+1;
+}
+}
+print "hash of names:\n", Dumper(\%seen);
